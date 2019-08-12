7c2127eef1139e9d95052b7d667ad34eb0fbeeb6
erv <ervinmitra@gmail.com>
2019-08-12 16:57:08 -0700

    initialized empty shells for team controller

---
+++ controllers/team-controllers.js
@@ -0,0 +1,24 @@
+const Team = require('../models/Team');
+
+const createOne = async(team, options) => {
+    // TODO
+};
+
+const createMany = async(teams, options) => {
+    // TODO
+};
+
+const readOne = async(options) => {
+    // TODO
+};
+
+const readMany = async(options) => {
+    // TODO
+};
+
+module.exports = {
+    createOne,
+    createMany,
+    readOne,
+    readMany,
+};
