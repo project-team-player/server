c37591de05c53748ed424dd6614198d328ede17a
erv <ervinmitra@gmail.com>
2019-08-12 16:43:26 -0700

    prototype team model complete

--- models/Team.js
+++ models/Team.js
@@ -6,6 +6,33 @@ const mongodbErrorHandler = require('mongoose-mongodb-errors');
 const teamSchema = new teamSchema({
     fullName: String,
     key: String,
+    placeName: String,
+    teamName: String,
+    conference: String,
+    division: String,
+    byeWeek: Number,
+    headCoach: String,
+    offensiveCoordinator: String,
+    defensiveCoordinator: String,
+    specialTeamsCoach: String,
+    wins: {
+        type: Number,
+        default: 0,
+    },
+    losses: {
+        type: Number,
+        default: 0,
+    },
+    draws: {
+        type: Number,
+        default: 0,
+    },
+    wikiLogoURL: String,
+    wikiWordMarkURL: String,
+    primaryColor: String,
+    secondaryColor: String,
+    tertiaryColor: String,
+    stadiumDetails: Object,
 });
 
 teamSchema.plugin(mongodbErrorHandler);
