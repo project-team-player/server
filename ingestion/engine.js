e4f816a1c568f2022f4f45d4109baa4bafee713f
erv <ervinmitra@gmail.com>
2019-08-13 09:06:48 -0700

    file mods

--- ingestion/engine.js
+++ ingestion/engine.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: __dirname + '../.env' });
 const fs = require('fs');
 
 if(process.argv.includes('--team')) {
-    // TODO
+    // TODO Run Team ingestion
     console.log('Running Ingestion Engine for Teams');
 } else {
     console.log('Need further arguments to specify engine to be run');
