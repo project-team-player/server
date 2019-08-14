c50aa2fce5359f5cc6c82608721fd70034a07618
erv <ervinmitra@gmail.com>
2019-08-14 10:10:16 -0700

    progess on game controller

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -1,15 +1,35 @@
 const Game = require('../models/Game');
 
+/**
+ * 
+ * @param {Object} game -> game to be added
+ * @param {Object} options -> optional parameters
+ * @returns {Object} returned Object
+ */
 const createOne = async(game, options) => {
-    // TODO
+    const returnAwait = await Game.create(game);
+    return returnAwait;
 };
 
+/**
+ * 
+ * @param {Object} games -> games to be added 
+ * @param {Object} options -> optional parameters
+ * @returns {Object} returned Object
+ */
 const createMany = async(games, options) => {
-    // TODO
+    const returnAwait = await Game.insertMany(games, { ordered: false });
+    return returnAwait;
 };
 
+/**
+ * 
+ * @param {Object} options -> defines parameters to find 
+ * @returns {Object} returned found object
+ */
 const readOne = async(options) => {
-    // TODO
+    const returnAwait = await Game.findOne(options);
+    return returnAwait;
 };
 
 const readMany = async(options) => {
