e9254b0c504c21d2ebda513089e721c9b444c3cc
erv <ervinmitra@gmail.com>
2019-08-14 10:05:45 -0700

    empty shells for game controller

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -1,1 +1,24 @@
 const Game = require('../models/Game');
+
+const createOne = async(game, options) => {
+    // TODO
+};
+
+const createMany = async(games, options) => {
+    // TODO
+};
+
+const readOne = async(options) => {
+    // TODO
+};
+
+const readMany = async(options) => {
+    // TODO
+};
+
+module.exports = {
+    createOne,
+    createMany,
+    readOne,
+    readMany,
+};
