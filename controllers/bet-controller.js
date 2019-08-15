23a98fae7c1d24a7bce72bef1f29378b461fe5ae
erv <ervinmitra@gmail.com>
2019-08-15 09:23:13 -0700

    initialized bet and comment controllers

---
+++ controllers/bet-controller.js
@@ -0,0 +1,24 @@
+const Bet = require('../models/Bet');
+
+const createOne = async(bet, options) => {
+    // TODO
+};
+
+const createMany = async(bets, options) => {
+    // TODO
+};
+
+const readOne = async(options) => {
+    // TFD
+};
+
+const readMany = async(options) => {
+    // TFD
+};
+
+module.exports = {
+    createOne,
+    createMany,
+    readOne,
+    readMany,
+};
---
+++ controllers/comment-controller.js
@@ -0,0 +1,25 @@
+const Comment = require('../models/Comment');
+
+const createOne = async(comment, options) => {
+    // TFD
+};
+
+const createMany = async(comments, options) => {
+    // TFD
+};
+
+const readOne = async(options) => {
+    // TFD
+};
+
+const readMany = async(options) => {
+    // TFD
+};
+
+
+module.exports = {
+    createOne,
+    createMany,
+    readOne,
+    readMany,
+}
