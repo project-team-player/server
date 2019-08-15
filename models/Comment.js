bb834b98f45bb369ab0e3b349f069eafa320c53d
erv <ervinmitra@gmail.com>
2019-08-15 08:51:41 -0700

    comment model

---
+++ models/Comment.js
@@ -0,0 +1,32 @@
+/** Comment Model */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+
+const commentSchema = new Schema({
+    owner: {
+        ownerID: String,
+        objectReference: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user',
+        },
+    },
+    comments: {
+        commentID: String,
+        objectReference: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'comment',
+        },
+    },
+    slicesToEnter: {
+        type: Number,
+        default: 4,
+    },
+});
+
+commentSchema.plugin(mongodbErrorHandler);
+
+const Comment = mongoose.model('comment', commentSchema);
+
+module.exports = Comment;
