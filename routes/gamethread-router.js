5f9aeb297d4ba98176eb3b9a4ea6ab92575b17fd
erv <ervinmitra@gmail.com>
2019-08-15 13:33:08 -0700

    router for gamethreads "get by week" complete

--- routes/gamethread-router.js
+++ routes/gamethread-router.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
 const { catchErrors } = require('../handlers/error-handlers');
+const gamethreadController = require('../controllers/gamethread-controller');
+const CustomError = require('../handlers/Custom-Error');
 
+const MAX_WEEK_REG = 17;
+const MIN_WEEK_REG = 1;
+
+// route to get all gamethreads based off week
+router.get('/week/:week',
+    catchErrors(async(req, res) => {
+        if(req.params.week >= MIN_WEEK_REG && req.params.week <= MAX_WEEK_REG) {
+            const gameThreads = await gamethreadController.readMany({ week: req.params.week });
+            return res.status(201).json({
+                message: `Week ${req.params.week} gamethreads`,
+                gameThreads,
+            });
+        } else {
+            const errorWeek = new CustomError(400, 'Invalid week, enter a week from 1 - 17');
+            return res.status(400).json({ errorWeek });
+        }
+    })
+);  
+
+
+module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -6,10 +6,12 @@ const express = require('express');
 const publicRouter = require('./public-router');
 const authRouter = require('./auth-router');
 const gameRouter = require('./game-router');
+const gamethreadRouter = require('./gamethread-router');
 
 const app = express();
 app.use('/', publicRouter);
 app.use('/authenticate', authRouter);
 app.use('/games', gameRouter);
+app.use('/gamethreads', gamethreadRouter);
 
 module.exports = app;
