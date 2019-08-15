3b26d3fbacdc65d9da426c108e89443b2c09c9ab
erv <ervinmitra@gmail.com>
2019-08-15 09:30:55 -0700

    CR in bet controller

--- controllers/bet-controller.js
+++ controllers/bet-controller.js
@@ -1,19 +1,45 @@
 const Bet = require('../models/Bet');
 
+/**
+ * 
+ * @param {Object} bet -> the bet object 
+ * @param {Object} options -> optional parameters
+ * @returns {Object} created object
+ */
 const createOne = async(bet, options) => {
-    // TODO
+    const returnAwait = await Bet.create(bet);
+    return returnAwait;
 };
 
+/**
+ * 
+ * @param {Array of Objects} bets -> bets to be inserted 
+ * @param {Object} options -> optional parameters
+ * @returns {Object} returned object
+ */
 const createMany = async(bets, options) => {
-    // TODO
+    const returnAwait = await Bet.insertMany(bets, { ordered: false });
+    return returnAwait;
 };
 
+/**
+ * 
+ * @param {Object} options -> defines parameters to find
+ * @returns {Object} -> found object
+ */
 const readOne = async(options) => {
-    // TFD
+    const returnAwait = await Bet.findOne(options);
+    return returnAwait;
 };
 
+/**
+ * 
+ * @param {Options} options -> defines parameters to find
+ * @returns {Options} -> found objects
+ */
 const readMany = async(options) => {
-    // TFD
+    const returnAwait = await Bet.find(options);
+    return returnAwait;
 };
 
 module.exports = {
