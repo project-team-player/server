cd3f986bc9c0ec05554a22d599d54c6105415a11
erv <ervinmitra@gmail.com>
2019-08-15 09:36:57 -0700

    CR on comment controller

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,19 +1,45 @@
 const Comment = require('../models/Comment');
 
+/**
+ * 
+ * @param {Object} comment -> comment to be created
+ * @param {Object} options -> optional parameters
+ * @returns {Object} created object
+ */
 const createOne = async(comment, options) => {
-    // TFD
+    const returnAwait = await Comment.create(comment);
+    return returnAwait;
 };
 
+/**
+ * 
+ * @param {Array of Objects} comments -> objects to be added 
+ * @param {Object} options -> optional parameters
+ * @returns {Object} returned objects 
+ */
 const createMany = async(comments, options) => {
-    // TFD
+    const returnAwait = await Comment.insertMany(comments, { ordered: false });
+    return returnAwait;
 };
 
+/**
+ * 
+ * @param {Object} options -> defines parameters on find
+ * @returns {Object} found object
+ */
 const readOne = async(options) => {
-    // TFD
+    const returnAwait = await Comment.findOne(options);
+    return returnAwait;
 };
 
+/**
+ * 
+ * @param {Object} options -> defines parameters on find
+ * @returns {Object} found objects
+ */
 const readMany = async(options) => {
-    // TFD
+    const returnAwait = await Comment.find(options);
+    return returnAwait;
 };
 
 
@@ -22,4 +48,4 @@ module.exports = {
     createMany,
     readOne,
     readMany,
-}
+};
