385334fbcebdc577d051aeb42247791473a99e41
erv <ervinmitra@gmail.com>
2019-08-15 13:59:42 -0700

    thread router to get obj from id

--- routes/gamethread-router.js
+++ routes/gamethread-router.js
@@ -10,10 +10,10 @@ const MIN_WEEK_REG = 1;
 router.get('/week/:week',
     catchErrors(async(req, res) => {
         if(req.params.week >= MIN_WEEK_REG && req.params.week <= MAX_WEEK_REG) {
-            const gameThreads = await gamethreadController.readMany({ week: req.params.week });
+            const gamethreads = await gamethreadController.readMany({ week: req.params.week });
             return res.status(201).json({
                 message: `Week ${req.params.week} gamethreads`,
-                gameThreads,
+                gamethreads,
             });
         } else {
             const errorWeek = new CustomError(400, 'Invalid week, enter a week from 1 - 17');
@@ -22,5 +22,19 @@ router.get('/week/:week',
     })
 );  
 
+// route to get gamethread based off game
+router.get('/:id',
+    catchErrors(async(req, res) => {
+        const gamethread = await gamethreadController.readOne({ _id: req.params.id });
+        if(gamethread) {
+            return res.status(201).json({ 
+                gamethread,
+            });
+        } else {
+            const errorID = new CustomError(400, 'No gamethread found based off that id');
+            return res.status(400).json({ errorID });
+        }
+    })
+);
 
 module.exports = router;
