8860f59b9dee265c5ef86bf6d5a19662be979f74
erv <ervinmitra@gmail.com>
2019-08-19 12:41:18 -0700

    dev mirror complete

---
+++ mirrors/production-mirror.js
@@ -0,0 +1,11 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '/../.env') });
+const mongoose = require('mongoose');
+
+const runMirror = async () => {
+
+};
+
+module.exports = {
+    runMirror,
+}
--- mirrors/proliferate.js
+++ mirrors/proliferate.js
@@ -1,14 +1,21 @@
 /**
  * This code mirrors data in DB in such order
  * test -> development -> staging -> production
+ * WARNING: Copies teams, games and gamethreads collections!!!!!!
  */
+const devMirror = require('./developement-mirror');
+const stagMirror = require('./staging-mirror');
+const prodMirror = require('./production-mirror');
 
 if(process.argv.includes("--dev")) {
     console.log("Mirror test DB into development DB");
+    devMirror.runMirror();
 } else if(process.argv.includes("--stag")) {
     console.log("Mirror development DB into staging DB");
+    stagMirror.runMirror();
 } else if(process.argv.includes("--prod")) {
     console.log("Mirror staging DB into production DB");
+    prodMirror.runMirror();
 } else {
     console.log("Mirror initialized");
 }
