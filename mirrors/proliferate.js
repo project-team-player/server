ef090d6b97d266a8eb62e2b84ec71b29b9c618b6
erv <ervinmitra@gmail.com>
2019-08-19 11:11:24 -0700

    initialized creation of mirrors

--- mirrors/proliferate.js
+++ mirrors/proliferate.js
@@ -2,3 +2,13 @@
  * This code mirrors data in DB in such order
  * test -> development -> staging -> production
  */
+
+if(process.argv.includes("--dev")) {
+    console.log("Mirror test DB into development DB");
+} else if(process.argv.includes("--stag")) {
+    console.log("Mirror development DB into staging DB");
+} else if(process.argv.includes("--prod")) {
+    console.log("Mirror staging DB into production DB");
+} else {
+    console.log("Mirror initialized");
+}
