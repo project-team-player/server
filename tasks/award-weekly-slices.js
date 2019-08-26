417b2cce1a50bc51720a31b01d23302c68eafa5b
erv <ervinmitra@gmail.com>
2019-08-26 10:11:46 -0700

    award weekly slices initiated

---
+++ tasks/award-weekly-slices.js
@@ -0,0 +1,8 @@
+/**
+ * Awards weekly slices to users. 
+ * Activates on :
+ *  -> User creation
+ *  -> Every close of the week.
+ * Desirable to be AUTOMATATED WEEKLY.
+ * Must know User Controller
+ */
