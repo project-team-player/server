b6729ae3cbe6051ec31ca8041d38f0c1cbd39a7b
erv <ervinmitra@gmail.com>
2019-08-28 09:55:31 -0700

    initialized users router and sync with main

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
+const passport = require('passport');
 const { catchErrors } = require('../handlers/error-handlers');
 const userController = require('../controllers/user-controller');
 const CustomError = require('../handlers/Custom-Error');
+
+// route that reads all bets from the P.O.V. of the user.
+router.get('/bets',
+    passport.authenticate('jwt', { session: false }),
+    catchErrors(async(req, res) => {
+        
+    })
+);
+
+module.exports = router;
