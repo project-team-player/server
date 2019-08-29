233aab3704077449d9a28f39ae1d2f09d1e8a6a4
erv <ervinmitra@gmail.com>
2019-08-29 09:29:43 -0700

    game scores resolvers

--- tasks/resolve-game-scores.js
+++ tasks/resolve-game-scores.js
@@ -1,7 +1,23 @@
 /**
- * Resolves the game scores with a given season and week
+ * Resolves the game scores with a given season and week.
+ * Calls the score-partition ingestion engine.
  */
-const scores = require('../ingestion/scores-partition');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '/../.env') });
+const mongoose = require('mongoose');
+const scores = require('../ingestion/score-partition');
 const gameController = require('../controllers/game-controller');
+
+const resolveScores = async (season, week) => {
+    try {
+        const gameScores = await scores.runEngine(season, week);
+    } catch(err) {
+        console.log(`Error has occured ${err}`);
+    }
+}
+
+module.exports = {
+    resolveScores,
+};
 
 
