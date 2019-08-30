e9fe7919b522ab9bf96999fab79126730318cc92
erv <ervinmitra@gmail.com>
2019-08-30 09:33:55 -0700

    logic for score resolving

--- tasks/resolve-game-scores.js
+++ tasks/resolve-game-scores.js
@@ -16,8 +16,28 @@ const resolveScores = async (season, week) => {
          * 1. Pull all game objects from the database by { week }.
          * 2. Match gameScores with the pulled games by { gameKey } (preferably), { globalGameID } (alternative).
          * 3. Update all the game objects to reflect winner.
+         * NOTE: gameScores has 'GameKey'. weekGames has 'gameKey'.
          */
-        
+        mongoose.connect(process.env.DATABASE_CONNECTION);
+        mongoose.Promise = global.Promise;
+        const weekGames = await gameController.readMany({ week });
+        for(let i = 0; i < weekGames.length; ++i) {
+            for(let j = 0; j < gameScores.length; ++j) {
+                if(weekGames[i].gameKey === gameScores[j].GameKey) {
+                    // then decide winner
+                    let winner = '';
+                    if(gameScores[j].AwayScore > gameScores[j].HomeScore) {
+                        winner = gameScores[j].AwayTeam;
+                    } else if(gameScores[j].AwayScore < gameScores[j].HomeScore) {
+                        winner = gameScores[j].HomeTeam;
+                    } else {
+                        winner = 'TIE';
+                    }
+                    // reflect winner into weekGame obj
+                    await gameController.updateOne(weekGames[i]._id, { winner });
+                }
+            }
+        }
     } catch(err) {
         console.log(`Error has occured ${err}`);
     }
