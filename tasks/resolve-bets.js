78d6e335ee7ce3336c22034f8bdb535e123e713a
erv <ervinmitra@gmail.com>
2019-08-30 12:15:33 -0700

    returned Obj expanded for bet resolvers

--- tasks/resolve-bets.js
+++ tasks/resolve-bets.js
@@ -17,6 +17,7 @@ const resolveBets = async (week) => {
     try {
         mongoose.connect(process.env.DATABASE_CONNECTION);
         mongoose.Promise = global.Promise;
+        let resolvedBets = 0;
         const games = await gameController.readMany({ week });
         for(let i = 0; i < games.length; ++i) {
             const bet = await betController.readOne({ slug: games[i].slug });
@@ -30,11 +31,21 @@ const resolveBets = async (week) => {
                 // the bet loses
                 isWin = false;
             }
-            // update the bet object
+            // update the bet object with its isWin key
             await betController.updateOne(bet._id, { isWin });
+            resolvedBets++;
         }
+        // breaking out this for loop means all bets associated with week's games
+        // have been updated and resolved. 
 
         // disconnect DB connection
+        mongoose.disconnect();
+        const returnObj = {
+            games: games.length,
+            resolved: resolvedBets,
+            message: `${resolvedBets} bets have been resolved in ${games.length} games.`,
+        };
+        return returnObj;
     } catch(err) {
         console.log(`Error has occured ${err}`);
     }
