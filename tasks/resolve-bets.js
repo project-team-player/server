321426383a8e4a4b68ba70b2fff19929ef02ce15
erv <ervinmitra@gmail.com>
2019-08-30 11:05:24 -0700

    changed POV of the bet resolver

--- tasks/resolve-bets.js
+++ tasks/resolve-bets.js
@@ -1,8 +1,8 @@
 /**
  * The bet resolver for every user. 
- * 1. Obtain all the users in the database
- * 2. Check every user's bets array. 
+ * 1. Obtain all the bets in the database
  *      -> For each bet, check gamethread, check game reference, pull the winner. 
  *      -> IF the winner is the same as in the bet, user wins that bet.
  *      -> ELSE user loses the bet. 
+ *      -> update that bet object's 'isWin' key.
  */
