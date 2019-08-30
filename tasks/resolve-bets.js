077ab7d8a7523deb1be958c14d7da19fb1fe12de
erv <ervinmitra@gmail.com>
2019-08-30 11:40:29 -0700

    injected game controller into bet resolver

--- tasks/resolve-bets.js
+++ tasks/resolve-bets.js
@@ -2,21 +2,22 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const mongoose = require('mongoose');
 const betController = require('../controllers/bet-controller');
+const gameController = require('../controllers/game-controller');
 
 /**
  * The bet resolver for every user. 
- * 1. Obtain all the bets in the database
- *      -> For each bet, check game reference (use SLUG), pull the winner. 
+ * 1. Obtain all the games in the database
+ *      -> For each game, find the associated bet (use SLUG), pull the winner. 
  *      -> IF the winner is the same as in the bet, user wins that bet.
  *      -> ELSE user loses the bet. 
  *      -> update that bet object's 'isWin' key
  * @returns {Object} with messages
  */
-const resolveBets = async => {
+const resolveBets = async (week) => {
     try {
         mongoose.connect(process.env.DATABASE_CONNECTION);
         mongoose.Promise = global.Promise;
-        const bets = await betController.readMany({});
+        const games = await gameController.readMany({ week });
 
         // disconnect DB connection
     } catch(err) {
