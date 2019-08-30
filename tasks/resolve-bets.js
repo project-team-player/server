ee3372adffa5e80ac40b4641bde7dd4e14985509
erv <ervinmitra@gmail.com>
2019-08-30 11:09:03 -0700

    plugged in badass bet controller

--- tasks/resolve-bets.js
+++ tasks/resolve-bets.js
@@ -10,3 +10,4 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const mongoose = require('mongoose');
+const betResolver = require('../controllers/bet-controller');
