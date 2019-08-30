d8e2533c50666dda746fee5204a5a89c5ed56c11
erv <ervinmitra@gmail.com>
2019-08-30 11:09:59 -0700

    export bet resolver

--- tasks/resolve-bets.js
+++ tasks/resolve-bets.js
@@ -11,3 +11,11 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const mongoose = require('mongoose');
 const betResolver = require('../controllers/bet-controller');
+
+const resolveBets = async => {
+    // TODO
+};  
+
+module.exports = {
+    resolveBets,
+};
