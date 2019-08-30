57091ab1d1b3b16b4429da366878a501c0ee6d31
erv <ervinmitra@gmail.com>
2019-08-30 11:24:05 -0700

    slug to ID games

--- tasks/resolve-bets.js
+++ tasks/resolve-bets.js
@@ -6,7 +6,7 @@ const betController = require('../controllers/bet-controller');
 /**
  * The bet resolver for every user. 
  * 1. Obtain all the bets in the database
- *      -> For each bet, check gamethread, check game reference, pull the winner. 
+ *      -> For each bet, check game reference (use SLUG), pull the winner. 
  *      -> IF the winner is the same as in the bet, user wins that bet.
  *      -> ELSE user loses the bet. 
  *      -> update that bet object's 'isWin' key
