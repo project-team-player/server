10b1b0b1017b537045a613ae9d72aa7b5d746faf
erv <ervinmitra@gmail.com>
2019-09-02 14:39:36 -0700

    weekly lose field

---
+++ tasks/add-weekly-loses.js
@@ -0,0 +1,21 @@
+/**
+ * Adds zero to weekly loses fields in all users
+ */
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '/../.env') });
+const mongoose = require('mongoose');
+const userController = require('../controllers/user-controller');
+
+const addWeeklyLoses = async (dbName) => {
+    // create DB conn string
+    const dbConnection = `${process.env.DB_CONN_STR1}${process.env.DATABASE_ROOT_USERNAME}${process.env.DB_CONN_STR2}${process.env.DATABASE_ROOT_PASSWORD}${process.env.DB_CONN_STR3}${dbName}${process.env.DB_CONN_STR4}`;
+    try {
+
+    } catch(err) {
+        console.log(`Error has occured ${err}`);
+    }
+};
+
+module.exports = {
+    addWeeklyLoses,
+};
