5f1b1687b93d6028f35d655ebaed265306a59d68
erv <ervinmitra@gmail.com>
2019-09-02 11:58:15 -0700

    ZERO on initial slices won on week

--- tasks/weekly-slices-won.js
+++ tasks/weekly-slices-won.js
@@ -9,8 +9,22 @@ const mongoose = require('mongoose');
 const userController = require('../controllers/user-controller');
 
 const distributeSlices = async (dbName) => {
-
+    // create DB string
+    const dbConnection = `${process.env.DB_CONN_STR1}${process.env.DATABASE_ROOT_USERNAME}${process.env.DB_CONN_STR2}${process.env.DATABASE_ROOT_PASSWORD}${process.env.DB_CONN_STR3}${dbName}${process.env.DB_CONN_STR4}`;
+    try {
+        mongoose.connect(dbConnection);
+        mongoose.Promise = global.Promise;
+        const result = await userController.updateMany({
+            pizzaSlicesWonWeek: 0,
+        });
+        mongoose.disconnect();
+        return result;
+    } catch(err) {
+        console.log(`Error has occured ${err}`);
+    }
 };
+
+distributeSlices(process.argv[2]);
 
 module.exports = {
     distributeSlices,
