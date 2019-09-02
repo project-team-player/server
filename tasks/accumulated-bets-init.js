53cc051202a3cc91b733610a6f20204fe9a46876
erv <ervinmitra@gmail.com>
2019-09-02 11:14:15 -0700

    accumulated bets array

---
+++ tasks/accumulated-bets-init.js
@@ -0,0 +1,29 @@
+/**
+ * Gives all users a default accumulated bets array
+ */
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '/../.env') });
+const mongoose = require('mongoose');
+const userController = require('../controllers/user-controller');
+
+const assignArray = async (dbName) => {
+    // create the DB string
+    const dbConnection = `${process.env.DB_CONN_STR1}${process.env.DATABASE_ROOT_USERNAME}${process.env.DB_CONN_STR2}${process.env.DATABASE_ROOT_PASSWORD}${process.env.DB_CONN_STR3}${dbName}${process.env.DB_CONN_STR4}`;
+    try {
+        mongoose.connect(dbConnection);
+        mongoose.Promise = global.Promise;
+        const result = await userController.updateMany({
+            accumulatedBets: [],
+        });
+        mongoose.disconnect();
+        return result;
+    } catch(err) {
+        console.log(`Error has occured ${err}`);
+    }
+};
+
+assignArray(process.argv[2]);
+
+module.exports = {
+    assignArray,
+};
