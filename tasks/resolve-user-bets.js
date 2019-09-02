4d961d75d78c044aa9660cacfa53105fd20fe2d1
erv <ervinmitra@gmail.com>
2019-09-02 11:50:51 -0700

    steps

--- tasks/resolve-user-bets.js
+++ tasks/resolve-user-bets.js
@@ -4,5 +4,7 @@
  * 1. Get all users in the DB
  * 2. read all their bets in their bets array 
  * 3. for each bet, look at the 'isWin' field
- * 4. IF 'isWin' is true, increment user's weeklyWins
+ * 4. IF 'isWin' is true, increment user's 'weeklyWins' AND add 
+ *  'slicesBet' on the bet into the user's 'pizzaSlicesWonWeek' field
+ * 5. ELSE 'isWin is false, do nothing.
  */
