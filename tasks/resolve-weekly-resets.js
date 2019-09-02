a51c028ad9c9319b5c6169fbba7dd1db527101a4
erv <ervinmitra@gmail.com>
2019-09-02 14:12:54 -0700

    initialized weekly reset resolver

---
+++ tasks/resolve-weekly-resets.js
@@ -0,0 +1,9 @@
+/**
+ * Fields that need to be reset weekly
+ * -----------------------------------------------------------------------------
+ * User
+ * -> 'bets' into 'accumulatedBets' then empty 'bets'
+ * -> 'pizzaSlicesWeekly' into 'pizzaSlicesTotal' then reset 
+ *    'pizzaSlicesWeekly' into 64
+ * -> 'weeklyWins' into 'wins' then reset 'weeklyWins' to 0
+ */
