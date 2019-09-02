09bfdbc4f320096c7afcf8ffff5b3f89d49d7982
erv <ervinmitra@gmail.com>
2019-09-02 16:17:59 -0700

    sync instructions to task manager

--- tasks/task-manager.js
+++ tasks/task-manager.js
@@ -5,3 +5,12 @@ if(process.argv.includes('--weekly-slices')) {
 } else {
     console.log('Task Manager Activated');
 }
+
+/**
+ * If a task pipeline is to be created, this will be the order
+ * 1. resolve-game-scores
+ * 2. resolve-bets
+ * 3. resolve-user-bets
+ * 4. resolve-user-awards
+ * 5. resolve-weekly-resets
+ */
