5a485f4aaf9a08931d626b6c7d81ce8e3b0e76df
erv <ervinmitra@gmail.com>
2019-09-09 11:44:48 -0700

    shell for gravatar generator

--- tasks/generate-gravatar.js
+++ tasks/generate-gravatar.js
@@ -9,7 +9,12 @@ const commentController = require('../controllers/comment-controller');
 
 const generate = async (dbName) => {
     // create the DB conn string
-    
+    const dbConnection = `${process.env.DB_CONN_STR1}${process.env.DATABASE_ROOT_USERNAME}${process.env.DB_CONN_STR2}${process.env.DATABASE_ROOT_PASSWORD}${process.env.DB_CONN_STR3}${dbName}${process.env.DB_CONN_STR4}`;
+    try {
+
+    } catch(err) {
+        console.log(`Error has occured ${err}`);
+    }
 };
 
 module.exports = {
