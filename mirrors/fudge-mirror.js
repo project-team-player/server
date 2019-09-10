7f4e414dd74e0e5d46e26d1652091950e6fcd40e
erv <ervinmitra@gmail.com>
2019-09-10 10:50:10 -0700

    fudge baked til brown and tender

--- mirrors/fudge-mirror.js
+++ mirrors/fudge-mirror.js
@@ -11,7 +11,19 @@ const runMirror = async () => {
     try {
         const copyFromDB = `${process.env.DB_CONN_STR1}${process.env.DATABASE_ROOT_USERNAME}${process.env.DB_CONN_STR2}${process.env.DATABASE_ROOT_PASSWORD}${process.env.DB_CONN_STR3}${process.env.DB_TEST}${process.env.DB_CONN_STR4}`;
         const copyToDB = `${process.env.DB_CONN_STR1}${process.env.DATABASE_ROOT_USERNAME}${process.env.DB_CONN_STR2}${process.env.DATABASE_ROOT_PASSWORD}${process.env.DB_CONN_STR3}${process.env.DB_FUDGE}${process.env.DB_CONN_STR4}`;
-        
+        await mongoose.connect(copyFromDB); 
+        mongoose.Promise = global.Promise;
+        const allUsers = await userController.readMany({});
+        const allGames = await gameController.readMany({});
+        const allGamethreads = await gamethreadController.readMany({});
+        const allBets = await betController.readMany({});
+        await mongoose.disconnect();
+        await mongoose.connect(copyToDB);
+        await userController.createMany(allUsers);
+        await gameController.createMany(allGames);
+        await gamethreadController.createMany(allGamethreads);
+        await betController.createMany(allBets);
+        await mongoose.disconnect();
     } catch(err) {
         console.log(`Error has occured ${err}`);
     }
