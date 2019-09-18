c5a6c30b69d031bff59160260972d1b884d984f4
erv <ervinmitra@gmail.com>
2019-09-18 16:55:10 -0700

    board model initialized

--- models/Board.js
+++ models/Board.js
@@ -6,3 +6,12 @@ const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 const mongodbErrorHandler = require('mongoose-mongodb-errors');
 
+const boardSchema = new Schema({
+
+});
+
+boardSchema.plugin(mongodbErrorHandler);
+
+const Board = mongoose.model('board', boardSchema);
+
+module.exports = Board;
