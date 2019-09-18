92675a7d732b61003e0c8c870d809c7863ee80f5
erv <ervinmitra@gmail.com>
2019-09-18 16:53:04 -0700

    dependencies added to the weekly leader board

--- models/WeeklyLeaderboard.js
+++ models/WeeklyLeaderboard.js
@@ -1,3 +1,8 @@
 /**
  * Model for a weekly leader board
  */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+
