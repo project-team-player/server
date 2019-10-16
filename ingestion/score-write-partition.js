530dfdbad80ff3499d8a60161707bea34864cb77
erv <ervinmitra@gmail.com>
2019-10-16 10:53:59 -0700

    runEngine initialized

--- ingestion/score-write-partition.js
+++ ingestion/score-write-partition.js
@@ -8,3 +8,11 @@
 const axios = require('axios');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
+
+const runEngine = async () => {
+
+};
+
+module.exports = {
+    runEngine,
+};
