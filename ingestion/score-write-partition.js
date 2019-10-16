41ad60a94cb001db56ef664532ec762beaa8023c
erv <ervinmitra@gmail.com>
2019-10-16 10:51:36 -0700

    initialized task file for writing scores

--- ingestion/score-write-partition.js
+++ ingestion/score-write-partition.js
@@ -2,9 +2,6 @@
  * Score Write -> Writes the scores in a given game.
  * Different 3rd party API. Actual scores are written.
  * Limited to 25 calls per day. 
- * Options on how to match score with a game:
- * 1. Obtain home and away teams (awayTeam.key and homeTeam.key) 
- *  -> match with data.data.events[i].teams_normalized[0].abbreviation (away)
- *      and data.data.events[i].teams_normalized[1].abbreviation (home)
  * This then gets called by a task program in the task folder.
+ * Namely ../tasks/write-actual-scores.js
  */
---
+++ tasks/write-actual-scores.js
@@ -0,0 +1,7 @@
+/**
+ * Calls the program in ../ingestion/score-write-partition.js.
+ * Options on how to match score with a game:
+ * 1. Obtain home and away teams (awayTeam.key and homeTeam.key) 
+ *  -> match with data.data.events[i].teams_normalized[0].abbreviation (away)
+ *      and data.data.events[i].teams_normalized[1].abbreviation (home)
+ */
