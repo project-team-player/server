e6a174f3130dbe0a85689d71a7832b782770686b
erv <ervinmitra@gmail.com>
2019-10-17 17:19:45 -0700

    findOneAndUpdate

--- tasks/write-actual-scores.js
+++ tasks/write-actual-scores.js
@@ -40,6 +40,4 @@ const writeScores = async (date, dbName, year, week) => {
     }
 };
 
-module.exports = {
-    writeScores,
-};
+writeScores(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
