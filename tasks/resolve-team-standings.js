764c92485339eef5883d8ba72e634b898f6952a2
erv <ervinmitra@gmail.com>
2019-10-24 12:35:01 -0700

    injected DB connection string

--- tasks/resolve-team-standings.js
+++ tasks/resolve-team-standings.js
@@ -18,8 +18,10 @@ const mongoose = require('mongoose');
 const teamController = require('../controllers/team-controller');
 
 const resolveStandings = async (week) => {
+    // create DB conn string
+    const dbConnection = `${process.env.DB_CONN_STR1}${process.env.DATABASE_ROOT_USERNAME}${process.env.DB_CONN_STR2}${process.env.DATABASE_ROOT_PASSWORD}${process.env.DB_CONN_STR3}${dbName}${process.env.DB_CONN_STR4}`;
     try {
-        
+
     } catch (err) {
         console.log(`Error has occured ${err}`);
     }
