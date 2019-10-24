fd65b42537533d3c92e469f2499bba9cba18459e
erv <ervinmitra@gmail.com>
2019-10-24 10:54:56 -0700

    initialized standing partition

---
+++ ingestion/standing-partition.js
@@ -0,0 +1,5 @@
+/**
+ * Pulls the standings from the 3rd-party API. Yes i know it could be calculated.
+ * But who has time for stuff like that.
+ * This function will most likely be inserted into the pipe1 flow.
+ */
