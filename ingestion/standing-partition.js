14aec7649f4eed08364d4920de3380d6ca871c8d
erv <ervinmitra@gmail.com>
2019-10-24 10:56:52 -0700

    initialized standings partition

--- ingestion/standing-partition.js
+++ ingestion/standing-partition.js
@@ -3,3 +3,6 @@
  * But who has time for stuff like that.
  * This function will most likely be inserted into the pipe1 flow.
  */
+const axios = require('axios');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '/../.env') });
