4e9102e4d7ffa2189f5989e6e091f02e509fbff9
erv <ervinmitra@gmail.com>
2019-10-30 09:35:00 -0700

    dates 1 array

--- data/game-dates.js
+++ data/game-dates.js
@@ -1,8 +1,31 @@
 /**
  * Back log of games from week 1 to week 6
+ * -> Thursday night games may or may not have final scores on Thursday, try pulling on FRIDAY instead
+ * -> Sunday games can be pulled on Sunday 
+ * -> BUT the LAST Sunday night game may not be included in the Sunday pull.Try pulling it on Monday instead.
+ * -> Monday night games may or may not have final scores on Monday, try pulling on Tuesday instead.
  */
-const dates = [
+const dates1 = [
+    ['2019-09-06', 1],
+    ['2019-09-08', 1],
+    ['2019-09-09', 1],
+    ['2019-09-10', 1],
+    ['2019-09-13', 2],
+    ['2019-09-15', 2],
+    ['2019-09-16', 2],
+    ['2019-09-17', 2],
+    ['2019-09-20', 3],
+    ['2019-09-22', 3],
+    ['2019-09-23', 3],
+    ['2019-09-24', 3],
 
 ];
 
-module.exports = dates;
+const dates2 = [
+
+];
+
+module.exports = {
+    dates1,
+    dates2,
+};
