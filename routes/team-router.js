3a939a82de4a45d77de719aea09e8531e361eabe
erv <ervinmitra@gmail.com>
2019-11-06 12:12:30 -0800

    changed _id to key, easier AF that way

--- routes/team-router.js
+++ routes/team-router.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const { catchErrors } = require('../handlers/error-handlers');
 const teamController = require('../controllers/team-controller');
 
-// route to get the team based off id
-router.get('/:id',
+// route to get the team based off key
+router.get('/:key',
     catchErrors(async(req, res) => {
-        const team = await teamController.readOne({ _id: req.params.id });
+        const team = await teamController.readOne({ key: req.params.key });
         return res.status(201).json({
             team
         });
