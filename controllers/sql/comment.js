fdfa874a0848e23f64bfca9e4c4899b0f6193a43
erverted <ervinmitra@gmail.com>
2023-05-17 20:50:59 -0500

    SQL queries for comments

---
+++ controllers/sql/comment.js
@@ -0,0 +1,31 @@
+const Comment = require('../models/Comment');
+const userController = require('./user-controller');
+const gamethreadController = require('./gamethread-controller');
+
+// create a comment 
+const createComment = async(comment, options) => {
+
+}
+
+// get comment
+const readComment = async(comment, options) => {
+
+}
+
+// edit comment
+const updateComment = async(comment, options) => {
+
+}
+
+// delete comment 
+const deleteComment = async(comment, options) => {
+
+}
+
+
+module.exports = {
+    createComment,
+    readComment,
+    deleteComment,
+    updateComment
+};
