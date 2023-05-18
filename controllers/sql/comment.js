fd2ed6850222b0027ff89ea89c38ce03962bebaa
erverted <ervinmitra@gmail.com>
2023-05-17 22:11:45 -0500

    write queries

--- controllers/sql/comment.js
+++ controllers/sql/comment.js
@@ -1,25 +1,58 @@
-const Comment = require('../models/Comment');
-const userController = require('./user-controller');
-const gamethreadController = require('./gamethread-controller');
 
 // create a comment 
-const createComment = async(comment, options) => {
-
+const createComment = async(comment) => {
+    [
+        id,
+        owner,
+        gravatar,
+        text,
+        createdAt,
+        isRootComment,
+        slug,
+        gameThreadReference
+    ] = [...comment]
+    const query = `INSERT INTO COMMENTS(
+        id,
+        owner,
+        gravatar,
+        text,
+        createdAt,
+        isRootComment,
+        slug,
+        gameThreadReference
+    ) VALUES (
+        ${id},
+        ${owner},
+        ${gravatar}.
+        ${text},
+        ${createdAt},
+        ${isRootComment},
+        ${slug},
+        ${gameThreadReference}
+    )
+    RETURNING *`
 }
 
 // get comment
-const readComment = async(comment, options) => {
-
+const readComment = async(comment) => {
+    const query = `SELECT * FROM COMMENTS
+    WHERE id = ${comment.id}
+    RETURNING *`
 }
 
 // edit comment
-const updateComment = async(comment, options) => {
-
+const updateComment = async(comment) => {
+    const query = `UPDATE COMMENTS
+    SET text = ${comment.text}
+    WHERE id = ${comment.id}
+    RETURNING *`
 }
 
 // delete comment 
 const deleteComment = async(comment, options) => {
-
+    const query = `DELETE FROM COMMENTS
+    WHERE id = ${comment.id}
+    RETURNING *`
 }
 
 
